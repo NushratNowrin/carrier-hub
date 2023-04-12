66f1aa76637776b948a47334077a2f4eef45af8e
NushratNowrin <nowrin2023@gmail.com>
2023-04-12 10:39:29 +0600

    responsive nav with hamberger done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'
-import ActiveLink from '../ActiveLink/ActiveLink';
+import React, { useState } from "react";
+import "./Navbar.css";
+import ActiveLink from "../ActiveLink/ActiveLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
-    return (
-        <div>
-            <div className='header flex justify-between items-center mx-48 my-12'>
+	const [open, setOpen] = useState(false);
+	return (
+		<nav>
+			<div className='header flex justify-between md:items-center md:mx-48 mx-4 md:my-12 my-2'>
 				<h2 className='brand-name text-3xl font-bold'>
-					<a href="/">CareerFolk</a>
+					<a href='/'>CareerFolk</a>
 				</h2>
-				<div className='menu'>
-					<ul className='flex gap-10 font-medium'>
-						<li>
-							<ActiveLink to='/statistics'>Statistics</ActiveLink>
-						</li>
-						<li>
-							<ActiveLink to='/applied-jobs'>Applied Jobs</ActiveLink>
-						</li>
-						<li>
-							<ActiveLink to='/blog'>Blog</ActiveLink>
-						</li>
-					</ul>
+				<div>
+					<div className='menu'>
+						{/* hambarger */}
+						<div onClick={() => setOpen(!open)} className='md:hidden'>
+							<span>
+								{open === true ? (
+									<XMarkIcon className='text-right absolute right-3 h-6 w-6 text-blue-500' />
+								) : (
+									<Bars3Icon className='h-6 w-6 text-blue-500' />
+								)}
+							</span>
+						</div>
+
+						{/* desktop icon */}
+						<div className={`md:flex justify-between items-center ${
+									open ? "" : "hidden"
+								}`}>
+							<ul
+								className="nav-ul md:flex gap-10 font-medium">
+								<li>
+									<ActiveLink to='/statistics'>Statistics</ActiveLink>
+								</li>
+								<li>
+									<ActiveLink to='/applied-jobs'>Applied Jobs</ActiveLink>
+								</li>
+								<li>
+									<ActiveLink to='/blog'>Blog</ActiveLink>
+								</li>
+							</ul>
+							<div>
+								<button className='nav-button px-5 py-3 rounded-md text-white font-semibold'>
+									Star Applying
+								</button>
+							</div>
+						</div>
+					</div>
 				</div>
-                <div>
-                    <button className='nav-button px-5 py-3 rounded-md text-white font-semibold'>Star Applying</button>
-                </div>
 			</div>
-        </div>
-    );
+		</nav>
+	);
 };
 
 export default Navbar;
